bcc54cf6d752382c5e1e3d2c44d7d0f093a25e8c
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-13 18:20:36 +0300

    KEITH

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
 global.read_status_from =  process.env.READ_STATUS_FROM  ||  "254710772666,254xxxxxxxx";
 
 global.api_smd = "https://api-smd-1.vercel.app"
-global.scan = "https://ibrahim-tech-1-4a7321f212d3.herokuapp.com/";
+global.scan = "https://keith-session-code-21b746de1312.herokuapp.com/";
 
 module.exports = {
 
